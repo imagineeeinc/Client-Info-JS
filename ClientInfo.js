16f73c44690ad359a91e6a761c5af11511f355d7
Imagineee <imagineeelego@gmail.com>
2020-10-04 18:28:36 +0100

    Add files via upload

---
+++ ClientInfo.js
@@ -0,0 +1,265 @@
+function LargeOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("BlackBerry", "BB10")!=-1) OSname = "BlackBerry";
+    if (navigator.appVersion.indexOf("FreeBSD")!=-1) OSname = "FreeBSD";
+    if (navigator.appVersion.indexOf("OS/2")!=-1) OSname = "OS/2";
+    if (navigator.appVersion.indexOf("Pocket PC")!=-1) OSname = "Pocket PC";
+    if (navigator.appVersion.indexOf("New Nintendo 3DS")!=-1) OSname = "New Nintedo 3DS";
+    if (navigator.appVersion.indexOf("Nintendo DSi")!=-1) OSname = "Nintedo DSi";
+    if (navigator.appVersion.indexOf("Nintendo 3DS")!=-1) OSname = "Nintedo 3DS";  
+    if (navigator.appVersion.indexOf("Nintendo Wii")!=-1) OSname = "Nintedo Wii";
+    if (navigator.appVersion.indexOf("Nintendo WiiU")!=-1) OSname = "Nintedo WiiU";
+    if (navigator.appVersion.indexOf("OpenBSD")!=-1) OSname = "OpenBSD";
+    if (navigator.appVersion.indexOf("Symbian", "Nokia_Series_40", "S60")!=-1) OSname = "Symbian";
+    if (navigator.appVersion.indexOf("PalmOS", "webOS")!=-1) OSname = "Palm";
+    if (navigator.appVersion.indexOf("SunOS")!=-1) OSname = "Solaris";
+    if (navigator.appVersion.indexOf("PLAYSTATION 3")!=-1) OSname = "PS 3";
+    if (navigator.appVersion.indexOf("PlayStation 4")!=-1) OSname = "PS 4";
+    if (navigator.appVersion.indexOf("PSP")!=-1) OSname = "psp";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("UNIX")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("WebTV OS")!=-1) OSname = "WebTV OS";
+    if (navigator.appVersion.indexOf("masking-agent")!=-1) OSname = "Mozzila Masking Agent";
+    if (navigator.appVersion.indexOf("HP-UX")!=-1) OSname = "Hewlett-Packard UniX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function MedOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("BlackBerry", "BB10")!=-1) OSname = "BlackBerry";
+    if (navigator.appVersion.indexOf("FreeBSD")!=-1) OSname = "FreeBSD";
+    if (navigator.appVersion.indexOf("OS/2")!=-1) OSname = "OS/2";
+    if (navigator.appVersion.indexOf("Pocket PC")!=-1) OSname = "Pocket PC";
+    if (navigator.appVersion.indexOf("OpenBSD")!=-1) OSname = "OpenBSD";
+    if (navigator.appVersion.indexOf("Symbian", "Nokia_Series_40", "S60")!=-1) OSname = "Symbian";
+    if (navigator.appVersion.indexOf("SunOS")!=-1) OSname = "Solaris";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("UNIX")!=-1) OSname = "UNIX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function SmallOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("FreeBSD")!=-1) OSname = "FreeBSD";
+    if (navigator.appVersion.indexOf("Symbian", "Nokia_Series_40", "S60")!=-1) OSname = "Symbian";
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("UNIX")!=-1) OSname = "UNIX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function ModernOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("UNIX")!=-1) OSname = "UNIX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function DeskOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("FreeBSD")!=-1) OSname = "FreeBSD";
+    if (navigator.appVersion.indexOf("OS/2")!=-1) OSname = "OS/2";
+    if (navigator.appVersion.indexOf("Pocket PC")!=-1) OSname = "Pocket PC";
+    if (navigator.appVersion.indexOf("OpenBSD")!=-1) OSname = "OpenBSD";
+    if (navigator.appVersion.indexOf("SunOS")!=-1) OSname = "Solaris";
+    if (navigator.appVersion.indexOf("UNIX")!=-1) OSname = "UNIX";
+    if (navigator.appVersion.indexOf("HP-UX")!=-1) OSname = "Hewlett-Packard UniX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function MobileOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("BlackBerry", "BB10")!=-1) OSname = "BlackBerry";
+    if (navigator.appVersion.indexOf("Pocket PC")!=-1) OSname = "Pocket PC";
+    if (navigator.appVersion.indexOf("Symbian", "Nokia_Series_40", "S60")!=-1) OSname = "Symbian";
+    if (navigator.appVersion.indexOf("PalmOS", "webOS")!=-1) OSname = "Palm";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function ConsoleOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("New Nintendo 3DS")!=-1) OSname = "New Nintedo 3DS";
+    if (navigator.appVersion.indexOf("Nintendo DSi")!=-1) OSname = "Nintedo DSi";
+    if (navigator.appVersion.indexOf("Nintendo 3DS")!=-1) OSname = "Nintedo 3DS";  
+    if (navigator.appVersion.indexOf("Nintendo Wii")!=-1) OSname = "Nintedo Wii";
+    if (navigator.appVersion.indexOf("Nintendo WiiU")!=-1) OSname = "Nintedo WiiU";
+    if (navigator.appVersion.indexOf("PLAYSTATION 3")!=-1) OSname = "PS 3";
+    if (navigator.appVersion.indexOf("PlayStation 4")!=-1) OSname = "PS 4";
+    if (navigator.appVersion.indexOf("PSP")!=-1) OSname = "psp";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function WeirdOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("BlackBerry", "BB10")!=-1) OSname = "BlackBerry";
+    if (navigator.appVersion.indexOf("FreeBSD")!=-1) OSname = "FreeBSD";
+    if (navigator.appVersion.indexOf("OS/2")!=-1) OSname = "OS/2";
+    if (navigator.appVersion.indexOf("Pocket PC")!=-1) OSname = "Pocket PC";
+    if (navigator.appVersion.indexOf("New Nintendo 3DS")!=-1) OSname = "New Nintedo 3DS";
+    if (navigator.appVersion.indexOf("Nintendo DSi")!=-1) OSname = "Nintedo DSi";
+    if (navigator.appVersion.indexOf("Nintendo 3DS")!=-1) OSname = "Nintedo 3DS";  
+    if (navigator.appVersion.indexOf("Nintendo Wii")!=-1) OSname = "Nintedo Wii";
+    if (navigator.appVersion.indexOf("Nintendo WiiU")!=-1) OSname = "Nintedo WiiU";
+    if (navigator.appVersion.indexOf("OpenBSD")!=-1) OSname = "OpenBSD";
+    if (navigator.appVersion.indexOf("Symbian", "Nokia_Series_40", "S60")!=-1) OSname = "Symbian";
+    if (navigator.appVersion.indexOf("PalmOS", "webOS")!=-1) OSname = "Palm";
+    if (navigator.appVersion.indexOf("SunOS")!=-1) OSname = "Solaris";
+    if (navigator.appVersion.indexOf("PLAYSTATION 3")!=-1) OSname = "PS 3";
+    if (navigator.appVersion.indexOf("PlayStation 4")!=-1) OSname = "PS 4";
+    if (navigator.appVersion.indexOf("PSP")!=-1) OSname = "psp";
+    if (navigator.appVersion.indexOf("WebTV OS")!=-1) OSname = "WebTV OS";
+    if (navigator.appVersion.indexOf("masking-agent")!=-1) OSname = "Mozzila Masking Agent";
+    if (navigator.appVersion.indexOf("HP-UX")!=-1) OSname = "Hewlett-Packard UniX";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+function MinOS() {
+    var OSname
+    if (navigator.appVersion.indexOf("Win")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) OSname = "MacOS";
+    if (navigator.appVersion.indexOf("Linux")!=-1) OSname = "Linux";
+    if (navigator.appVersion.indexOf("Windows")!=-1) OSname = "Windows";
+    if (navigator.appVersion.indexOf("Android")!=-1) OSname = "Android";
+    if (navigator.appVersion.indexOf("iPhone")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPod")!=-1) OSname = "ios";
+    if (navigator.appVersion.indexOf("iPad")!=-1) OSname = "ios";
+
+    //this is for firefox os
+    //if (navigator.appVersion.indexOf("")!=-1) OSname = "Firefox OS";
+    
+    //document.getElementById("box").innerHTML = OSname;
+    //document.getElementById("box").innerHTML = navigator.appVersion;
+    return OSname
+}
+
+function cijs(what) {
+    var info={
+        timeOpened: "new Date()",
+        timezone: "(new Date()).getTimezoneOffset()/60",
+        pageon: "return window.location.pathname ",
+        referrer: "return document.referrer ",
+        previousSites: "return history.length ",
+    
+        browserName: "return navigator.appName ",
+        browserEngine: "return navigator.product ",
+        browserVersion1a: "return navigator.appVersion ",
+        browserVersion1b: "return navigator.userAgent ",
+        browserLanguage: "return navigator.language ",
+        browserOnline: "return navigator.onLine ",
+        browserPlatform: "return navigator.platform ",
+        javaEnabled: "return navigator.javaEnabled() ",
+        dataCookiesEnabled: "return navigator.cookieEnabled ",
+        dataCookies1: "return document.cookie ",
+        dataCookies2: "return decodeURIComponent(document.cookie.split(';')) ",
+        dataStorage: "return localStorage ",
+    
+        sizeScreenW: "return screen.width ",
+        sizeScreenH: "return screen.height ",
+        sizeDocW: "return document.width ",
+        sizeDocH: "return document.height ",
+        sizeInW: "return innerWidth ",
+        sizeInH: "return innerHeight ",
+        sizeAvailW: "return screen.availWidth ",
+        sizeAvailH: "return screen.availHeight ",
+        scrColorDepth: "return screen.colorDepth ",
+        scrPixelDepth: "return screen.pixelDepth ",
+    
+        latitude: "return position.coords.latitude ",
+        longitude: "return position.coords.longitude ",
+        accuracy: "return position.coords.accuracy ",
+        altitude: "return position.coords.altitude ",
+        altitudeAccuracy: "return position.coords.altitudeAccuracy ",
+        heading: "return position.coords.heading ",
+        speed: "return position.coords.speed ",
+        timestamp: "return position.timestamp ",
+    };
+    var get = info[what]
+    var F = new Function (get);
+    //console.log(F)
+    
+    return(F());
+    //return (send());
+}
